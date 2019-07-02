55ef1f0551f5d5cb75c692d7dd883981aaef7c14
Erick HL <dev.erick95@gmail.com>
2019-07-01 23:29:25 -0500

    Página estática terminada v 1.0.0

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,13 +1,30 @@
-class User {
-	constructor(username) {
-		this.username = username
-	}
+// Navbar
 
-	getUsername() {
-		return this.username
+const hamburger = document.getElementById('hamburger')
+const navbar = document.getElementById('navbar')
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('is-active')
+	navbar.classList.toggle('d-inline-block')
+})
+
+// Controla si la barra de navegación debe ser fija
+const header = document.getElementById('header')
+
+window.onscroll = function () {
+	if (window.scrollY >= 200) {
+		header.style.background = '#fff'
+		header.style.borderBottom = '1px solid rgba(79,83,86,0.1)'
+	} else {
+		header.style.background = '#FAFAFC'
+		header.style.border = 'none'
 	}
 }
 
-const user1 = new User('coderick')
+// Sweetbox
 
-console.log(user1.getUsername())
+const gallery_container = document.getElementById('projects-list')
+
+sweetbox(gallery_container, {
+	descriptionImage: true
+})
